1698e343bb22e493d1c31b99c4114e305cac6a41
guilhermegonzatto22 <115480503+guilhermegonzatto22@users.noreply.github.com>
2022-10-10 11:59:09 -0300

    Update principal.js

--- principal.js
+++ principal.js
@@ -41,3 +41,11 @@ if(altura <= 0 || altura >= 3.00) {
 if(pesoEhValido && alturaEhValida) {
    var imc = peso / ( altura * altura); 
 }
+
+var setorTI = true;
+var gerente = true;
+if(setorTI == true || gerente == true){
+    console.log("Acesso permitido");
+}else{
+    console.log("Acesso negado")
+}
