490ab665ee84a64f26ae693247d6adebc30e4df3
guilhermegonzatto22 <115480503+guilhermegonzatto22@users.noreply.github.com>
2022-10-10 11:57:31 -0300

    Create principal.js

---
+++ principal.js
@@ -0,0 +1,43 @@
+alert(78 / (1.71 * 1.71));
+
+var vendas = 10;
+var clientesAbordados = 25;
+var mediaTentativasPorCliente = 4;
+
+var taxa = 0;
+
+taxa = vendas / clientesAbordados *  mediaTentativasPorCliente;
+console.log(taxa);
+
+
+var trPaciente = document.querySelector('#primeiro-paciente');
+var tdPeso = trPaciente.querySelector('.info-peso');
+var tdAltura = trPaciente.querySelector('.info-altura');
+
+var peso = tdPeso.textContent;
+var altura = tdAltura.textContent;
+
+var imc = peso / ( altura * altura);
+
+var trPaciente = document.querySelector('#primeiro-paciente');
+var tdPeso = trPaciente.querySelector('.info-peso');
+var tdAltura = trPaciente.querySelector('.info-altura');
+var tdImc = trPaciente.querySelector('.info-imc');
+
+var peso = tdPeso.textContent;
+var altura = tdAltura.textContent;
+
+var pesoEhValido = true; // assumindo de boa fé que o peso é válido
+var alturaEhValida = true; // assumindo de boa fé que a altura é válida
+
+if(peso <=0 || peso >= 1000) {
+   pesoEhValido = false;
+}
+
+if(altura <= 0 || altura >= 3.00) {
+  alturaEhValida = false;
+}
+
+if(pesoEhValido && alturaEhValida) {
+   var imc = peso / ( altura * altura); 
+}
