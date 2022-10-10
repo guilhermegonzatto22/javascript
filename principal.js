0a44f2827a20dfb8aa8d2df9d4b4087cd0d6d69b
guilhermegonzatto22 <115480503+guilhermegonzatto22@users.noreply.github.com>
2022-10-10 12:10:39 -0300

    Update principal.js

--- principal.js
+++ principal.js
@@ -116,4 +116,10 @@ var porcentagemCategoria = (despesaCategoria/rendaTotal) * 100;
 
 console.log(porcentagemCategoria);
 
+var produtos = document.querySelector("lista-produtos");
 
+for( var i=0 ; i < produtos.length ; i++){
+    var produto  = produtos[i];
+    var nomeDoProduto = produto.textContent;
+    console.log(nomeDoProduto);
+}
